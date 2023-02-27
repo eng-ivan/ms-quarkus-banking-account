27d8f7bd601a3b4f2d9aea20b1ecfa8ddf71def4
Ivan Santos <eng.ivancarlos85@gmail.com>
2023-02-27 13:47:03 -0300

    create container test

--- src/test/java/core/ics/AccountResourceTest.java
+++ src/test/java/core/ics/AccountResourceTest.java
@@ -1,21 +1,44 @@
 package core.ics;
 
+import core.ics.repository.AccountRepository;
+import core.ics.service.AccountService;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import javax.inject.Inject;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+
 import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class AccountResourceTest {
 
+    @Inject
+    AccountRepository accountRepository;
     @Test
-    public void testHelloEndpoint() {
+    void listAccount() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .when()
+                .get("/api/account/list")
+                .then()
+                .statusCode(OK.getStatusCode());
+    }
+
+    @Test
+    void findAccountByID() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 1)
+                .when()
+                .get("/api/account/{id}")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .body("agency", is(accountRepository.findById(1L).getAgency()))
+                .body("account", is(accountRepository.findById(1L).getAccount()));
     }
 
 }
---
+++ src/test/java/core/ics/InitializerTestContainers.java
@@ -0,0 +1,38 @@
+package core.ics;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+//@QuarkusTestResource(InitializerTestContainers.class)
+public class InitializerTestContainers implements QuarkusTestResourceLifecycleManager {
+
+    private PostgreSQLContainer postgreSQLContainer;
+
+    private Map<String, String> config(){
+        final Map<String, String> config = new HashMap<>();
+        config.put("quarkus.datasource.jdbc.url",postgreSQLContainer.getJdbcUrl());
+        config.put("quarkus.datasource.username",postgreSQLContainer.getUsername());
+        config.put("quarkus.datasource.password",postgreSQLContainer.getPassword());
+
+        return config;
+    }
+
+    @Override
+    public Map<String, String> start() {
+        this.postgreSQLContainer = new PostgreSQLContainer<>("postgres:11");
+        this.postgreSQLContainer.start();
+        return config();
+    }
+
+    @Override
+    public void stop() {
+        if (postgreSQLContainer != null){
+            postgreSQLContainer.close();
+        }
+    }
+}
